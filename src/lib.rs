105fd4bc571431ff265c8881e667ef9a8bb9d506
xavier-kong <kongxianchengxavier@gmail.com>
2023-10-20 21:22:05 +0800

    finish 4.4 tex

--- src/lib.rs
+++ src/lib.rs
@@ -13,37 +13,15 @@ fn main() {
         Alive = 1,
     }
 
+    #[wasm_bindgen]
     pub struct Universe {
         width: u32,
         height: u32,
         cells: Vec<Cell>,
     }
 
+    #[wasm_bindgen]
     impl Universe {
-        pub fn new() -> Universe {
-            let width = 64;
-            let height = 64;
-
-            let cells = (0..width * height)
-                .map(|i| {
-                    if i % 2 == 0 || i % 7 == 0 {
-                        Cell::Alive
-                    } else {
-                        Cell::Dead
-                    }
-                })
-            .collect();
-
-            Universe {
-                width,
-                height,
-                cells,
-            }
-        }
-
-        pub fn render(&self) -> String {
-            self.to_string()
-        }
         fn get_index(&self, row: u32, column: u32) -> usize {
             (row * self.width + column) as usize
         }
@@ -96,6 +74,43 @@ fn main() {
             }
 
             self.cells = next;
+        }
+
+        pub fn new() -> Universe {
+            let width = 64;
+            let height = 64;
+
+            let cells = (0..width * height)
+                .map(|i| {
+                    if i % 2 == 0 || i % 7 == 0 {
+                        Cell::Alive
+                    } else {
+                        Cell::Dead
+                    }
+                })
+            .collect();
+
+            Universe {
+                width,
+                height,
+                cells,
+            }
+        }
+
+        pub fn render(&self) -> String {
+            self.to_string()
+        }
+
+        pub fn width(&self) -> u32 {
+            self.width
+        }
+
+        pub fn height(&self) -> u32 {
+            self.height
+        }
+
+        pub fn cells(&self) -> *const Cell {
+            self.cells.as_ptr()
         }
     }
 
